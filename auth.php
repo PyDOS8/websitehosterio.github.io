dfdb098b3ab86434f2656fdf7dcd4ceb105ae30d
Websitehosterio <124309800+Websitehosterio@users.noreply.github.com>
2023-10-05 21:19:03 -0400

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,4 +1,24 @@
 /* PHP code to authenticate user */
 <?php
+  $tries = 0;
   echo "Authenticating User";
+  $username = $_GET["username"];
+  $password = $_GET["password"];
+  if(!$_COOKIE["banned"]){
+    if($username == $_COOKIE["username"] && $password == $_COOKIE["password"]){
+        echo "Authenticated!";
+    }else{
+      if($tries < 3){
+        echo "Incorrect username or password!";
+        $tries+=1;
+      }else{
+        echo "Try again in an hour!";
+        $cookie_name = "banned";
+        $cookie_value = "1hr";
+        setcookie($cookie_name, $cookie_value); /* set banned cookie */
+      }
+    }
+  }else{
+    echo "Try again in 1 hour";
+  }
 ?>
