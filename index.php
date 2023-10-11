13ebbaf58cf1680aeecd8d6e3ad1fa0387f0bc81
Websitehosterio <124309800+Websitehosterio@users.noreply.github.com>
2023-10-10 21:09:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -59,13 +59,13 @@ This is a website that will tell users about overdue work, get teacher feedback,
 	    <!-- Feedback Section -->
 	    <div class="course-feedback" id="course-feedback">
 		    <!-- Div for marks -->
-        	    <div class="marks" id="marks">
+        	    <div class="marks" id="marks">                                                                                
 					<p>Marks will go here</p>
 				</div>
 
         	    <!-- Feedback -->
-			    <div class="feedback" id="feedback">
-					<p>Teacher feedback will go here</p>
+		    <div class="feedback" id="feedback">
+			    <p>Teacher feedback will go here</p>
         	    </div>
       	    </div>
     	</div>
